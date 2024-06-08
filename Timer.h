8cecbe2f62e1b05d3c784a7e0347e68db0a4aed0
Kamila Grońska <gronskakamila@gmail.com>
2024-06-08 18:19:09 +0200

    dodanie Timera

---
+++ Timer.h
@@ -0,0 +1,30 @@
+//
+// Created by grons on 08.06.2024.
+//
+
+#ifndef OIAK_PROJEKT_TIMER_H
+#define OIAK_PROJEKT_TIMER_H
+#include <iostream>
+#include <chrono>
+using namespace std::chrono;
+
+
+class Timer {
+
+    high_resolution_clock:: time_point t0;
+    high_resolution_clock:: time_point t1;
+public:
+    void start() {
+        t0 = high_resolution_clock::now();
+    }
+    void stop() {
+        t1 = high_resolution_clock::now();
+    }
+
+    uint64_t timeperiod() {
+        duration <double> finalTime = t1 - t0;
+        return (uint64_t)(finalTime.count() * 1000);
+    }
+};
+
+#endif //OIAK_PROJEKT_TIMER_H
